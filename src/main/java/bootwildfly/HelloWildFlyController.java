a51671b76f8f9ee0be2de78503a73fa06ff96bfd
Madhusudan Chaganthi <15344551+aubie96@users.noreply.github.com>
2019-05-30 12:46:32 -0400

    now hooking up the insult generator with a restful call

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -6,14 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWildFlyController {
 
+	@RequestMapping("hello")
+	public String sayHello() {
+		return ("Hello, SpringBoot on Wildfly");
+	}
 
-    @RequestMapping("hello")
-    public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
-    }
+	@RequestMapping("hello2")
+	public String sayHelloAgain() {
+		return ("Hello, SpringBoot on Wildfly by Madhu");
+	}
 
-   @RequestMapping("hello2")
-    public String sayHelloAgain(){
-        return ("Hello, SpringBoot on Wildfly by Madhu");
-    }
-}	
+	@RequestMapping("insult")
+	public String getInsult() {
+		return new InsultGenerator().generateInsult();
+	}
+}
